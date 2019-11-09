64a994074369000cbff88b324afaf0b82a334d11
Bruno Weiler Foletto <brunoweiller@hotmail.com>
2019-11-09 17:22:35 -0300

    Arruamando sizeof

--- helpinho.h
+++ helpinho.h
@@ -1,9 +1,10 @@
 /*****fv0001*****/
-float functionSomarVetor(float valores[], int tamanho){
+float functionSomarVetor(float valores[]){
 	float total = 0;
-  for(int i = 0; i < tamanho; ++i){
-    total+=(float)(valores[i]);
-  }
+	int tamanho = sizeof(valores) / sizeof(valores[0]);
+	for(int i = 0; i < tamanho; ++i){
+		total+=(float)(valores[i]);
+	}
 	return total;
 }
 
@@ -78,47 +79,47 @@ void scnS(char s[]){
 
 /*****fv0002*****/
 float functionRetornarMaiorNumero(float valores[], int tam){
-  float maior;
-  int trocou = 0;
-  while(trocou != 1){
-    trocou = 1;
-    for(int z = 0; z < tam - 1; z++){
-      if(valores[z] < valores[z + 1]){
-        float aux;
-        aux = valores[z];
-        valores[z] = valores[z + 1];
-        valores[z + 1] = aux;
-        trocou = 0;
-      }
-    }
-  }
-  maior = valores[0];
-  return maior;
+	float maior;
+	int trocou = 0;
+	while(trocou != 1){
+		trocou = 1;
+		for(int z = 0; z < tam - 1; z++){
+			if(valores[z] < valores[z + 1]){
+				float aux;
+				aux = valores[z];
+				valores[z] = valores[z + 1];
+				valores[z + 1] = aux;
+				trocou = 0;
+			}
+		}
+	}
+	maior = valores[0];
+	return maior;
 }
 
 /*****fv0003*****/
 float functionRetornarMenorNumero(float valores[],int tamanho){
-  float maior;
-  int trocou = 0;
-  while(trocou != 1){
-    trocou = 1;
-    for(int z = 0; z < tamanho - 1; z++){
-      if(valores[z] > valores[z + 1]){
-        float aux;
-        aux = valores[z];
-        valores[z] = valores[z + 1];
-        valores[z + 1] = aux;
-        trocou = 0;
-      }
-    }
-  }
-  maior = valores[0];
-  return maior;
+	float maior;
+	int trocou = 0;
+	while(trocou != 1){
+		trocou = 1;
+		for(int z = 0; z < tamanho - 1; z++){
+			if(valores[z] > valores[z + 1]){
+				float aux;
+				aux = valores[z];
+				valores[z] = valores[z + 1];
+				valores[z + 1] = aux;
+				trocou = 0;
+			}
+		}
+	}
+	maior = valores[0];
+	return maior;
 }
 
 /*****fe0001*****/
 float functionConverterParaFahrenheit(float celsius){
-  float fahren;
-  fahren = (((float)celsius / 5.0) * 9.0) + 32;
-  return fahren;
+	float fahren;
+	fahren = (((float)celsius / 5.0) * 9.0) + 32;
+	return fahren;
 }
--- main.c
+++ main.c
@@ -5,16 +5,12 @@
 
 int main()
 {
-  float z, y[3] = {5.7, 5.7, 6.345};
-  z = functionRetornarMaiorNumero(y, 3);
-  prf(z, 3);
-  pr("\n");
-  z = functionRetornarMenorNumero(y, 3);
-  prf(z, 3);
-  pr("\n");
-  z = 24;
-  z = functionConverterParaFahrenheit(z);
-  prf(z, 3);
+
+	float z, y[3] = {5.7, 5.7, 6.345};
+	int x;
+	z = functionSomarVetor(y);
+	x = sizeof(y)/4;
+	prf(x, 3);
 
 	return 0;
 }
