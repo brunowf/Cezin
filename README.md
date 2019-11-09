dddb244b922be720b421baa661debb9a5fb33fae
Bruno Weiler Foletto <bruno.wf1998@aluno.ifsc.edu.br>
2019-11-09 03:35:44 +0000

    adicionando README e colocando códigos para funções

---
+++ README.md
@@ -0,0 +1,17 @@
+
+/*
+helpinho contém funções e procedimentos para facilitar a vida do programador C
+#procedimentos básicos
+p00001--pr(string)/*****recebe e imprimi uma string*****/
+p00002--prf(float, int)/*****recebe um número X e Y casas após a vírgula, então imprime X.Yf*****/
+p00003--scnS(string)/*****recebe uma string, scanea ela e muda o seu valor*****/
+#funções básicas
+f00001--scnF()/*****scanea float e return no mesmo*****/
+f00002--scnI()/*****scanea int e return no mesmo*****/
+#funções com vetores
+fv0001--functionSomarVetor(float[], int)/*****recebe um vetor e seu tamanho, então soma todos os valores do mesmo*****/
+fv0002--functionRetornarMaiorNumero(float[], int)/*****recebe um vetor e seu tamanho, então retorna o seu maior valor*****/
+fv0003--functionRetornarMenorNumero(float[], int)/*****recebe um vetor e seu tamanho, então retorna o seu menor valor*****/
+#funções específicas
+fe0001--functionConverterParaFahrenheit(float)/*****recebe um float X, então converte e retorna o seu valor em fahrenheit a partir do princípio que X está em celsius*****/
+*/
--- helpinho.h
+++ helpinho.h
@@ -1,73 +1,83 @@
-float somar(float num1, float num2){//somar 2 valores
-	float total;
-	total = num1 + num2;
+/*****fv0001*****/
+float functionSomarVetor(float valores[], int tamanho){
+	float total = 0;
+  for(int i = 0; i < tamanho; ++i){
+    total+=(float)(valores[i]);
+  }
 	return total;
 }
 
-void pr(char string[200]){//imprimir uma string
+/*****p00001*****/
+void pr(char string[]){
 	printf("%s", string);
 }
 
-void prf(float numero, int virgula){// numero = o número que vai ser impresso, virgula = quantidade de números após a virgula
+/*****p00002*****/
+void prf(float numero, int virgula){
 	switch(virgula){
 		case 0:
-		printf("%.0f", numero);
+		printf("%.0f", (float)numero);
 		break;
 
 		case 1:
-		printf("%.1f", numero);
+		printf("%.1f", (float)numero);
 		break;
 
 		case 2:
-		printf("%.2f", numero);
+		printf("%.2f", (float)numero);
 		break;
 
 		case 3:
-		printf("%.3f", numero);
+		printf("%.3f", (float)numero);
 		break;
 
 		case 4:
-		printf("%.4f", numero);
+		printf("%.4f", (float)numero);
 		break;
 
 		case 5:
-		printf("%.5f", numero);
+		printf("%.5f", (float)numero);
 		break;
 
 		case 6:
-		printf("%.6f", numero);
+		printf("%.6f", (float)numero);
 		break;
 
 		case 7:
-		printf("%.7f", numero);
+		printf("%.7f", (float)numero);
 		break;
 
 		case 8:
-		printf("%f", numero);
+		printf("%f", (float)numero);
 		break;
 
 		default:
-		printf("%f", numero);
+		printf("%f", (float)numero);
 		break;
 	}
 }
 
-float scnF(){//fazer um scan de um float e retornar o mesmo
+/*****f00001*****/
+float scnF(){
 	float numero;
 	scanf("%f", &numero);
 	return numero;
 }
 
-int scnI(){//fazer um scan de um integer e retornar o mesmo
+/*****f00002*****/
+int scnI(){
 	int numero;
 	scanf("%d", &numero);
 	return numero;
 }
 
-void scnS(char s[]){//recebe uma string, scanea ela e muda o seu valor
+/*****p00003*****/
+void scnS(char s[]){
 	scanf(" %s", s);
 }
-float maior(float valores[], int tam){//recebe um vetor de floats e o tamanho, retorna o malor valor do
+
+/*****fv0002*****/
+float functionRetornarMaiorNumero(float valores[], int tam){
   float maior;
   int trocou = 0;
   while(trocou != 1){
@@ -84,4 +94,31 @@ float maior(float valores[], int tam){//recebe um vetor de floats e o tamanho, r
   }
   maior = valores[0];
   return maior;
+}
+
+/*****fv0003*****/
+float functionRetornarMenorNumero(float valores[],int tamanho){
+  float maior;
+  int trocou = 0;
+  while(trocou != 1){
+    trocou = 1;
+    for(int z = 0; z < tamanho - 1; z++){
+      if(valores[z] > valores[z + 1]){
+        float aux;
+        aux = valores[z];
+        valores[z] = valores[z + 1];
+        valores[z + 1] = aux;
+        trocou = 0;
+      }
+    }
+  }
+  maior = valores[0];
+  return maior;
+}
+
+/*****fe0001*****/
+float functionConverterParaFahrenheit(float celsius){
+  float fahren;
+  fahren = (((float)celsius / 5.0) * 9.0) + 32;
+  return fahren;
 }
